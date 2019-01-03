775591cd946f0c25189b0b04b595e4a1496163fe
Dário Freire <dario.freire@gmail.com>
2019-01-03 21:52:00 +0000

    fetching some data with apollo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import uuid from "uuid/v4";
+import AppoloClient from "apollo-boost";
+import gql from "graphql-tag";
+import { ApolloProvider, ApolloConsumer } from "react-apollo";
+
+const client = new AppoloClient({
+  uri: "https://fakerql.com/graphql"
+});
 
 const SHOW = {
   ALL: "ALL",
@@ -81,6 +88,27 @@ class App extends React.Component {
           <input type="text" value={addText} onChange={this.onChangeText} />
           <button onClick={this.add}>Add</button>
         </div>
+        <ApolloProvider client={client}>
+          <h1>Apollo!!</h1>
+          <ApolloConsumer>
+            {client => {
+              client
+                .query({
+                  query: gql`
+                    {
+                      allTodos {
+                        id
+                        title
+                        completed
+                      }
+                    }
+                  `
+                })
+                .then(result => console.log("result", result));
+              return null;
+            }}
+          </ApolloConsumer>
+        </ApolloProvider>
       </div>
     );
   }
